f72abc75f27d189f37521b591c5dbc8d0ab89f2e
TarunSingh-07 <tarunsinghtech@gmail.com>
2026-07-30 11:11:42 +0530

    Add Question and QuestionService classes for quiz functionality

---
+++ Quiz_Console_App/Main.java
@@ -0,0 +1,8 @@
+package Quiz_Console_App;
+
+public class Main {
+    public static void main(String []a) {
+        QuestionService service = new QuestionService();
+        service.displayQuestions();
+    }
+}
---
+++ Quiz_Console_App/QuestionService.java
@@ -0,0 +1,10 @@
+package Quiz_Console_App;
+
+public class QuestionService {
+    Question[] questions = new Question[5];
+
+    public void displayQuestions() {
+        System.out.println("in DIsplay");
+    }
+
+}
