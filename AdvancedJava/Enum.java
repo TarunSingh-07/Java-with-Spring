baa277f4a98b6a9958b937003a8d22fd47864722
TarunSingh-07 <tarunsinghtech@gmail.com>
2026-08-16 00:27:05 +0530

    Add Enum demonstrating the use of enums in Java with a sample implementation

---
+++ AdvancedJava/Enum.java
@@ -0,0 +1,21 @@
+// Enum is a special class that represents a group of constants (unchangeable variables, like final variables).
+
+package AdvancedJava;
+
+enum Status {
+    Running, Pending, Failed, Success      //If you've scenarion where you want to use set of constants then you can use enum. 
+}
+
+public class Enum {
+    public static void main(String[] a) {
+        
+        Status s = Status.Pending;
+        System.out.println(s);
+
+        //Print all the values of the enum
+        Status [] ss = Status.values();   //values() method returns an array containing all of the values of the enum in the order they are declared. 
+        for(Status s1 : ss) {
+            System.out.println(s1 + " : " + s1.ordinal());
+        }
+    }
+}
